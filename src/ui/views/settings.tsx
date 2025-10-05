802cc143e2122a85f1f7a5b6b7e6239ed8921767
chiragtirth <chiragtirth.dev@gmail.com>
2025-10-05 14:10:06 -0500

    Update settings.tsx

--- src/ui/views/settings.tsx
+++ src/ui/views/settings.tsx
@@ -21,18 +21,6 @@ export default function Settings() {
 					title="About"
 				/>
 				<ListItem
-					link='https://eclipseemu.me/faq.html'
-					external
-					media={<i className="f7-icons">question_circle_fill</i>}
-					title="Help"
-				/>
-				<ListItem
-					link='https://github.com/iGBAEmu/EclipseIssues'
-					external
-					media={<i className="f7-icons">logo_github</i>}
-					title="Issue Tracker"
-				/>
-				<ListItem
 					link='/skins'
 					media={<i className="f7-icons">layers_fill</i>}
 					title="Skins"
@@ -81,41 +69,7 @@ export default function Settings() {
 					}}
 				/>
 			</List>
-			<ListFooter>Backups contain all your Eclipse data (minus ROMs and tokens).</ListFooter>
-
-			{/* Social */}
-
-			<ListTitle title="Social" />
-			<List>
-				<ListItem
-					link='https://twitter.com/tryeclipse'
-					external
-					media={<LazyImage src="https://api.zenithdevs.com/eclipse/twitter/twitter" />}
-					title="Twitter"
-					subtitle="The official Twitter account"
-				/>
-				<ListItem
-					link='https://reddit.com/r/eclipseemu'
-					external
-					media={<LazyImage src="https://api.zenithdevs.com/eclipse/twitter/reddit" />}
-					title="Reddit"
-					subtitle="The official subreddit"
-				/>
-				<ListItem
-					link='https://discord.gg/QcX8FQR'
-					external
-					media={<LazyImage src="https://api.zenithdevs.com/eclipse/twitter/discord" />}
-					title="Discord"
-					subtitle="The official Discord server"
-				/>
-				<ListItem
-					link='https://github.com/zenithdevs/eclipse'
-					external
-					media={<LazyImage src="https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/4b/75/74/4b757442-8ff0-1bcb-dfde-8d39fba370c4/AppIcon-0-1x_U007emarketing-0-7-0-85-220.png/246x0w.png" />}
-					title="GitHub"
-					subtitle="The official git repo"
-				/>
-			</List>
+			<ListFooter>Backups contain all your Frosix data (minus ROMs and tokens).</ListFooter>
 		</Page>
 	);
 }
